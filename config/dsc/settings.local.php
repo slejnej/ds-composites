d0230053dbdcd634565dafdf334eb8df31448c3f
Jernej Slejko <slejnej@gmail.com>
2024-09-18 10:26:40 +0200

    multisite

---
+++ config/dsc/settings.local.php
@@ -0,0 +1,40 @@
+<?php
+$settings['hash_salt'] = 'PtXEvtTeUkb6pXYyePdJED0Ryz-2G9NjJF6eGdjnKMGz6glCpGtloT1RD3tN09qfBG8yEdg6nQ';
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/development.services.yml';
+
+$settings['entity_update_batch_size'] = 50;
+$settings['entity_update_backup'] = TRUE;
+
+$settings['config_sync_directory'] = '../config/sync-dsc';
+$databases['default']['default'] = array (
+  'database' => 'drupal10',
+  'username' => 'drupal10',
+  'password' => 'drupal10',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+$settings['skip_permissions_hardening'] = TRUE;
+
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+$config['system.logging']['error_level'] = 'verbose';
+$settings['file_private_path'] = '/app/web/sites/default/private';
+$settings['file_temp_path'] = '/tmp';
+
+$settings['trusted_host_patterns'] = [
+  '^localhost$',
+  '127\.0\.0\.1',
+  '^ds-composites\.lndo\.site$'
+];
+
---
+++ web/sites/sites.php
@@ -0,0 +1,9 @@
+<?php
+
+// phpcs:ignoreFile
+
+$sites['ds-composites.lndo.site'] = 'ds-composites';
+$sites['ds-composites.2cent2.eu'] = 'ds-composites';
+
+$sites['ds-machining.lndo.site'] = 'ds-machining';
+$sites['ds-machining.2cent2.eu'] = 'ds-machining';
